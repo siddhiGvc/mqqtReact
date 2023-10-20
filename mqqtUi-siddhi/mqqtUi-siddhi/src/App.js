096e2f674d0f9930c9c15cc53614285341f67fe2
siddhiGvc <siddhi@gvc.in>
2023-10-20 13:10:21 +0530

    added

---
+++ mqqtUi-siddhi/mqqtUi-siddhi/src/App.js
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import { useState } from 'react';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function CustomizedTables() {
+  const [data,setData]=useState([])
+
+   React.useEffect(()=>{
+
+    // fetch('http://localhost:8080/transactions')
+    // .then((res)=>{
+    //   return res.json();
+    // })
+    // .then((json)=>{
+    //   console.log(json);
+    //   setData(json);
+      
+      
+    // })
+       
+      
+
+   },[])
+     
+  
+
+  return <>
+  <div styles={{width:'100%',padding:"30px",paddingTop:"40px"}}>
+  <div style={{marginTop:"50px",marginBottom:"30px"}}>
+  <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Select Start Date"
+        />
+        <TextField
+          
+          id="outlined-disabled"
+          label="Required"
+          defaultValue="Select End Date"
+        />
+        <TextField
+          id="outlined-password-input"
+          label="From"
+          type="text"
+          autoComplete="From"
+        />
+         <TextField
+          id="outlined-password-input"
+          label="To"
+          type="text"
+          autoComplete="To"
+        />
+  </div>
+    <TableContainer component={Paper} style={{widht:"80%"}}>
+      <Table sx={{ minWidth: 600 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+            <StyledTableCell align="right">Calories</StyledTableCell>
+            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </div>
+    </>
+}
