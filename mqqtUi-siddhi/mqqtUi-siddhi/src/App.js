bd3776237afd38381c978ab73479a451c620132b
siddhiGvc <siddhi@gvc.in>
2023-10-21 12:27:30 +0530

    filters_added

--- mqqtUi-siddhi/mqqtUi-siddhi/src/App.js
+++ mqqtUi-siddhi/mqqtUi-siddhi/src/App.js
@@ -9,6 +9,8 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import TablePagination from '@mui/material/TablePagination';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
 import axios from 'axios';
 import { useState } from 'react';
 
@@ -48,6 +50,27 @@ export default function CustomizedTables() {
   const [data,setData]=useState([])
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [startDate,setStartDate]=React.useState();
+  const [endDate,setEndDate]=React.useState();
+  const [from,setFrom]=React.useState("");
+  const [to,setTo]=React.useState("");
+
+  function formatStartDate(date) {
+    setStartDate(date);
+    // const year =date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+    // const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure 2-digit month
+    // const day = String(date.getDate()).padStart(2, '0'); // Ensure 2-digit day
+    // setStartDate(`${year}/${month}/${day}`);
+  }
+  
+  function formatEndDate(date) {
+    setEndDate(date);
+    // const year =date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+    // const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure 2-digit month
+    // const day = String(date.getDate()).padStart(2, '0'); // Ensure 2-digit day
+    // setEndDate(`${year}/${month}/${day}`);
+  }
+
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -58,9 +81,10 @@ export default function CustomizedTables() {
     setPage(0);
   };
 
-   React.useEffect(()=>{
-
-    fetch('http://localhost:8080/transactions')
+   const OnSubmit=()=>{
+    if(!from && !to)
+    {
+    fetch(`http://localhost:8080/transactions?start_date=${startDate}&end_date=${endDate}`)
     .then((res)=>{
       return res.json();
     })
@@ -70,40 +94,104 @@ export default function CustomizedTables() {
       
       
     })
+  }
+  else if(from && to)
+  {
+    fetch(`http://localhost:8080/transactions/serial?start_date=${startDate}&end_date=${endDate}&from=${from}&to=${to}`)
+    .then((res)=>{
+      return res.json();
+    })
+    .then((json)=>{
+      console.log(json);
+      setData(json);
+      
+      
+    })
+ 
+  }
+  else if(from)
+  {
+    fetch(`http://localhost:8080/transactions/from?start_date=${startDate}&end_date=${endDate}&from=${from}`)
+    .then((res)=>{
+      return res.json();
+    })
+    .then((json)=>{
+      console.log(json);
+      setData(json);
+      
+      
+    })
+ 
+  }
+  else if(to)
+  {
+    fetch(`http://localhost:8080/transactions/machine?start_date=${startDate}&end_date=${endDate}&to=${to}`)
+    .then((res)=>{
+      return res.json();
+    })
+    .then((json)=>{
+      console.log(json);
+      setData(json);
+      
+      
+    })
+ 
+  }
+
+   }
+
        
       
 
-   },[])
+   
      
   
 
   return <>
   <div styles={{width:'80%',padding:"30px",paddingTop:"40px",margin:"auto"}}>
   <div style={{marginTop:"50px",marginBottom:"30px"}}>
+  <FormControl style={{display:'flex',flexDirection:"row",justifyContent:'space-evenly'}} >
+    
   <TextField
+          type='date'
           required
           id="outlined-required"
-          label="Required"
-          defaultValue="Select Start Date"
+          label="Start Date "
+          InputLabelProps={{
+            shrink: true,
+          }}
+          
+          onChange={(e)=>formatStartDate(e.target.value)}
         />
         <TextField
-          
+          type="date"
+          required
           id="outlined-disabled"
-          label="Required"
-          defaultValue="Select End Date"
+          label="End Date "
+          InputLabelProps={{
+            shrink: true,
+          }}
+          
+          onChange={(e)=>formatEndDate(e.target.value)}
         />
         <TextField
           id="outlined-password-input"
           label="From"
           type="text"
           autoComplete="From"
+          onChange={(e)=>setFrom(e.target.value)}
         />
          <TextField
           id="outlined-password-input"
           label="To"
           type="text"
           autoComplete="To"
+          onChange={(e)=>setTo(e.target.value)}
         />
+         <Button type="submit" onClick={OnSubmit} variant="contained">Search</Button>
+         
+         </FormControl>
+        
   </div>
   <Paper  sx={{ width: '100%', overflow: 'hidden' }}>
     <TableContainer component={Paper} style={{widht:"50%",margin:"auto"}}>
@@ -114,6 +202,7 @@ export default function CustomizedTables() {
             <StyledTableCell align="left">Machine</StyledTableCell>
             <StyledTableCell align="left">Command</StyledTableCell>
             <StyledTableCell align="left">P1</StyledTableCell>
+            <StyledTableCell align="left">CreatedAt</StyledTableCell>
         
           </TableRow>
         </TableHead>
@@ -126,6 +215,7 @@ export default function CustomizedTables() {
               <StyledTableCell align="left">{row.machine}</StyledTableCell>
               <StyledTableCell align="left">{row.command}</StyledTableCell>
               <StyledTableCell align="left">{row.p1}</StyledTableCell>
+              <StyledTableCell align="left">{row.createdAt}</StyledTableCell>
             
             </StyledTableRow>
           ))}
